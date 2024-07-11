0c7cf93911a543394cedea26a48a7737587322f6
tolstovr <tolstovrob@gmail.com>
2024-07-11 07:20:56 +0400

    Feat (Quote): Added Quote component

---
+++ src/components/Quote/Quote.tsx
@@ -0,0 +1,20 @@
+import React from "react"
+import style from "./Quote.module.scss"
+
+interface QuoteProps {
+    text: string
+    author: string
+}
+
+const Quote = ({ text, author }: QuoteProps) => {
+  return (
+    <div className={style["quote-container"]}>
+      <blockquote className={style["quote"]}>
+        <p>{text}</p>
+        <cite className={style["author"]}>- {author}</cite>
+      </blockquote>
+    </div>
+  )
+}
+
+export default Quote
