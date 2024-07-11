a983d6b94f105f49de27953e46bb04496469374b
tolstovr <tolstovrob@gmail.com>
2024-07-11 06:59:42 +0400

    Fix (Section): Fixed props for Section module

--- src/modules/Section/Section.tsx
+++ src/modules/Section/Section.tsx
@@ -2,16 +2,16 @@ import React from "react"
 import style from "./Section.module.scss"
 
 interface SectionProps {
-    id: string
-    children: React.ReactNode
+    id?: string
+    children?: React.ReactNode
 }
 
 const Section = ({ id, children }: SectionProps) => {
   return (
     <>
-      <div className={style.anchor} id={id} />
+      <div className={style.anchor} id={id ?? ""} />
       <section>
-        {children}
+        {children ?? ""}
       </section>
     </>
   )
