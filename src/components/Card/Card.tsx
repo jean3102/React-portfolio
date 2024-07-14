4d435e9b9efbfb9d1dd655f5700b70074343c26a
tolstovr <tolstovrob@gmail.com>
2024-07-14 16:14:18 +0400

    Feat: Added Card component

---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,34 @@
+"use client"
+import React from "react"
+import style from "./Card.module.scss"
+import Image from "next/image"
+import Badge from "../Badge/Badge"
+import { getColorByTech } from "@/utils/brands"
+
+interface CardProps {
+  imagePath?: string
+  title?: string
+  description?: string
+  tags?: string[]
+  difficulty?: number
+  controls?: React.ReactNode
+}
+
+const Card = (props: CardProps) => {
+  return (
+    <div className={style.card}>
+      <Image src={props.imagePath ?? "/projects/default.webp"} alt={props.title ?? "An empty card"} width={324} height={192} className={style.image} />
+      <div className={style.text}>
+        <h2>{props.title}</h2>
+        <p>{props.description}</p>
+      </div>
+      <div className={style.tags}>
+        {props.tags?.map((tag, index) => (
+          <Badge key={index} color={getColorByTech(tag)} size="small">{tag}</Badge>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default Card
