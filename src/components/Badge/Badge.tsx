8f49746086bcb82b699cb0e7c89baa6189865828
tolstovr <tolstovrob@gmail.com>
2024-07-14 16:12:23 +0400

    Feat: Added customizable Badge component

---
+++ src/components/Badge/Badge.tsx
@@ -0,0 +1,21 @@
+// @ts-nocheck
+import React from "react"
+import style from "./Badge.module.scss"
+
+interface BadgeProps {
+  color?: string
+  size?: "small" | "medium" | "large"
+  children?: React.ReactNode
+}
+
+const Badge = ({color, size, children}: BadgeProps) => {
+  return (
+    <span className={`${style.badge} ${style[size ?? ""] ?? style.default}`} style={{"--color": color}}>
+      {children}
+    </span>
+  )
+}
+
+export default Badge
+
+// Note: I used ts-nocheck because I needed to pass color to --color in my scss. Don't put a blame on me!
