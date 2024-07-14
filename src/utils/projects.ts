7f829e453222438c91495692630443c3487a2028
tolstovr <tolstovrob@gmail.com>
2024-07-14 11:30:34 +0400

    Utils: Added projects.ts
    There's static info about my projects

---
+++ src/utils/projects.ts
@@ -0,0 +1,37 @@
+export const projectsList = [
+    {
+        imagePath: "@skills/react.svg",
+        title: "Some React stuff",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur velit vel met tell tellus et",
+        tags: ["React", "JavaScript", "TypeScript"],
+        link: "https://github.com/tolstovr"
+    },
+    {
+        imagePath: "@skills/react.svg",
+        title: "Some React stuff",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur velit vel met tell tellus et",
+        tags: ["React", "JavaScript", "TypeScript"],
+        link: "https://github.com/tolstovr"
+    },
+    {
+        imagePath: "@skills/react.svg",
+        title: "Some React stuff",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur velit vel met tell tellus et",
+        tags: ["React", "JavaScript", "TypeScript"],
+        link: "https://github.com/tolstovr"
+    },
+    {
+        imagePath: "@skills/react.svg",
+        title: "Some React stuff",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur velit vel met tell tellus et",
+        tags: ["React", "JavaScript", "TypeScript"],
+        link: "https://github.com/tolstovr"
+    },
+    {
+        imagePath: "@skills/react.svg",
+        title: "Some React stuff",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur velit vel met tell tellus et",
+        tags: ["React", "JavaScript", "TypeScript"],
+        link: "https://github.com/tolstovr"
+    }
+]
