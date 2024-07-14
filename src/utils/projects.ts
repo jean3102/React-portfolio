a2d7b159859dac549b310cafbacdeeffcd3f2328
tolstovr <tolstovrob@gmail.com>
2024-07-14 14:24:21 +0400

    Utils: Small fixes in projects.ts

--- src/utils/projects.ts
+++ src/utils/projects.ts
@@ -1,37 +1,26 @@
 export const projectsList = [
     {
-        imagePath: "@skills/react.svg",
+        imagePath: "/skills/react.svg",
         title: "Some React stuff",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur velit vel met tell tellus et",
         tags: ["React", "JavaScript", "TypeScript"],
-        link: "https://github.com/tolstovr"
+        link: "https://github.com/tolstovr",
+        difficulty: 5,
     },
     {
-        imagePath: "@skills/react.svg",
+        imagePath: "/skills/react.svg",
         title: "Some React stuff",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur velit vel met tell tellus et",
         tags: ["React", "JavaScript", "TypeScript"],
-        link: "https://github.com/tolstovr"
+        link: "https://github.com/tolstovr",
+        difficulty: 8,
     },
     {
-        imagePath: "@skills/react.svg",
+        imagePath: "/skills/react.svg",
         title: "Some React stuff",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur velit vel met tell tellus et",
         tags: ["React", "JavaScript", "TypeScript"],
-        link: "https://github.com/tolstovr"
-    },
-    {
-        imagePath: "@skills/react.svg",
-        title: "Some React stuff",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur velit vel met tell tellus et",
-        tags: ["React", "JavaScript", "TypeScript"],
-        link: "https://github.com/tolstovr"
-    },
-    {
-        imagePath: "@skills/react.svg",
-        title: "Some React stuff",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur velit vel met tell tellus et",
-        tags: ["React", "JavaScript", "TypeScript"],
-        link: "https://github.com/tolstovr"
+        link: "https://github.com/tolstovr",
+        difficulty: 3,
     }
 ]
